e446688e3b9780c211fc312fdd4f4813532b6ccd
shinnji <ec2-user@ip-172-31-21-242.ec2.internal>
2021-08-12 15:16:40 +0000

    meshiterro-lesson3-finish

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '196e968892a53e7fc12e9f3fe7936d41cbfb50780ccb675123f104c9ce6723923ba8322e2df4d3d3745f56649ebf23391d3c6cf2765c7a82eb59bdf2f781e7c1'
